063d15d1c86e78a2003b5e6433537359fdc38d29
Mihaela Cristina LUNGU <mihaela.cristina.lungu@ibm.com>
2020-06-14 14:10:11 +0200

    [wip] upload -> add remote method for uploading files, hide all default loopback APIs

--- server/models/doc.js
+++ server/models/doc.js
@@ -2,4 +2,33 @@
 
 module.exports = function(Doc) {
 
+  Doc.compressMultipleFiles = async function(ctx) {
+
+  };
+
+  Doc.remoteMethod('compressMultipleFiles', {
+    accepts: [
+      {
+        arg: 'ctx',
+        type: 'object',
+        http: function (ctx) {
+          return ctx;
+        },
+      }
+    ],
+    returns: [
+      {
+        arg: 'object',
+        root: true,
+      },
+    ],
+    description: 'Method to upload multiple files and archive them',
+    name: 'Compress files',
+    http: {
+      verb: 'post',
+      path: '/',
+    },
+  });
 };
+    
+
