81d3e0c2d9f330b238285096a77b8ac6a181d18d
Mihaela Cristina LUNGU <mihaela.cristina.lungu@ibm.com>
2020-06-17 01:41:52 +0200

    [wip] archive -> add logic to call the methods  to add archive to COS, add userId for archiveFile method, update API response, add env var for COS bucket name

--- server/models/doc.js
+++ server/models/doc.js
@@ -2,20 +2,29 @@
 
 const debug = require('debug')('server:zip-file-manager:doc');
 debug.log = console.log.bind(console);
+
 const HelperService = require('../services/helpers.service');
+const ARCHIVES_BUCKET_NAME = process.env.ARCHIVES_BUCKET_NAME;
 
 module.exports = function (Doc) {
 
-  Doc.compressMultipleFiles = async function (ctx) {
+  Doc.archiveMultipleFiles = async function (ctx, res) {
     try {
       debug('Starting compresing multiple files upload');
+      const userId = 'Miha'; // to do: add token and take the userId
       const files = await HelperService.getFilesFromRequest(ctx.req);
       HelperService.validateFilesSize(files);
       HelperService.validateArchiveSize(files);
-      debug('Succesfuly uploaded %d file', files.length);
-      const zipArchive = await HelperService.archiveFiles(files);
 
-      return zipArchive;
+      debug('Succesfully uploaded %d file', files.length);
+      const zipArchiveName = await HelperService.archiveFiles(files, userId);
+
+      debug('Saving archive into Cloud Object Storage');
+      const cos = HelperService.configObjectStorage();
+      const uploadedFilePath = await HelperService.uploadFileToCOS(cos, ARCHIVES_BUCKET_NAME, zipArchiveName);
+      debug('Succesfully uploaded archive file to Cloud Object Storage - path: %s for userId: %s', uploadedFilePath, userId);
+
+      return HelperService.configAPIResponse(res, zipArchiveName);
 
     } catch (error) {
       console.error(error);
@@ -23,7 +32,7 @@ module.exports = function (Doc) {
     };
   };
 
-  Doc.remoteMethod('compressMultipleFiles', {
+  Doc.remoteMethod('archiveMultipleFiles', {
     accepts: [
       {
         arg: 'ctx',
@@ -31,6 +40,13 @@ module.exports = function (Doc) {
         http: function (ctx) {
           return ctx;
         },
+      },
+      {
+        arg: 'response',
+        type: 'object',
+        http: {
+          source: 'res',
+        }
       }
     ],
     returns: [
@@ -40,7 +56,7 @@ module.exports = function (Doc) {
       },
     ],
     description: 'Method to upload multiple files and archive them',
-    name: 'Compress files',
+    name: 'Archive files',
     http: {
       verb: 'post',
       path: '/',
